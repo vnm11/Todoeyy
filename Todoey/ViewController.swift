8c33c942964bfde5e2eaf258f631034ce241b0d3
Vinicius Serban <vinimiclea@viniciuss-imac.local>
2018-12-20 12:38:49 +0200

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Vinicius Miclea Serban on 20/12/2018.
+//  Copyright © 2018 Vinicius Miclea Serban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
